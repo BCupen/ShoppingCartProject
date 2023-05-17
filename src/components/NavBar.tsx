5b69fa12eea83b5a89feb2e2424d47dfe2d2358e
BCupen <bacupen@gmail.com>
2023-05-17 14:28:14 -0400

    Revert "completed"
    This reverts commit 441961a4fbf5eaad4af9eceaa0266a8acda1cbba.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,10 @@
-import { HStack, Link, Heading, IconButton, Circle } from "@chakra-ui/react";
+import { HStack, Link, Heading, IconButton } from "@chakra-ui/react";
 import { CartIcon } from "../assets/Icons";
 import { NavBarProps } from "../interfaces";
-import { Link as RouterLink } from "react-router-dom";
-import { useContext } from "react";
-import { CartContext } from "./CartProvider";
+import { Link as RouterLink} from "react-router-dom";
 
-export default function NavBar({ onOpen }: NavBarProps) {
-  const [cart] = useContext(CartContext);
+
+export default function NavBar({onOpen}: NavBarProps) {
   return (
     <HStack spacing="67px" color="brand.cream">
       <Link
@@ -18,50 +16,27 @@ export default function NavBar({ onOpen }: NavBarProps) {
       >
         <Heading fontSize="32px">Home</Heading>
       </Link>
-      <Link
-        as={RouterLink}
-        to="/shop"
-        _hover={{
-          color: "brand.tan",
-        }}
-      >
+      <Link as={RouterLink} to='/shop' _hover={{
+        color: 'brand.tan'
+      }}>
         <Heading fontSize="32px">Shop</Heading>
       </Link>
-      <Link
-        as={RouterLink}
-        to="/contact"
-        _hover={{
-          color: "brand.tan",
-        }}
-      >
+      <Link _hover={{
+        color: 'brand.tan'
+      }}>
         <Heading fontSize="32px">Contact</Heading>
       </Link>
-      <HStack>
-        <IconButton
-          aria-label="Your Cart"
-          icon={<CartIcon />}
-          bg="brand.charcoal"
-          border="none"
-          _hover={{
-            bg: "brand.charcoal",
-            border: "none",
-          }}
-          onClick={onOpen}
-        ></IconButton>
-        {cart.length > 0 && (
-          <Circle
-            size="25px"
-            bgColor="brand.cartCount"
-            color="brand.charcoal"
-            fontFamily="Righteous"
-            position='absolute'
-            top='20px'
-            right='0'
-          >
-            {cart.length}
-          </Circle>
-        )}
-      </HStack>
+      <IconButton
+        aria-label="Your Cart"
+        icon={<CartIcon />}
+        bg="brand.charcoal"
+        border='none'
+        _hover={{
+            bg:'brand.charcoal',
+            border: 'none'
+        }}
+        onClick={onOpen}
+      ></IconButton>
     </HStack>
   );
 }
